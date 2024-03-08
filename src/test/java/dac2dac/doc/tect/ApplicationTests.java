2c9f6cc773a206bf297d1dc5258f9fd71cdff432
jyjyjy25 <jyjyjy7418@gmail.com>
2024-03-08 15:03:13 +0900

    Setting: 프로젝트 환경 세팅 (#1)

---
+++ src/test/java/dac2dac/doc/tect/ApplicationTests.java
@@ -0,0 +1,13 @@
+package dac2dac.doc.tect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
