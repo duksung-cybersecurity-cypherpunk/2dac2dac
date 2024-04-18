f4de7bc44e8c2db42c17eea8e201422b5e06655e
jyjyjy25 <jyjyjy7418@gmail.com>
2024-04-19 01:50:56 +0900

    Refactor: 외부 API 각각의 내부 데이터 조회를 비동기 방식으로 수행 (#4)

--- src/main/java/dac2dac/doctect/agency/service/HospitalService.java
+++ src/main/java/dac2dac/doctect/agency/service/HospitalService.java
@@ -5,9 +5,6 @@ import dac2dac.doctect.agency.repository.HospitalRepository;
 import dac2dac.doctect.agency.vo.HospitalItems;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.MediaType;
@@ -30,52 +27,28 @@ public class HospitalService {
     private String HOSPITAL_ENDPOINT;
 
     @Async
-    public void saveAllHospitalInfo() throws ParseException {
-        String hospitalInfo = webClient.get()
-            .uri(uriBuilder -> uriBuilder
-                .path(HOSPITAL_ENDPOINT)
-                .queryParam("serviceKey", HOSPITAL_API_KEY)
-                .queryParam("_type", "json")
-                .build())
-            .retrieve()
-            .bodyToMono(String.class)
-            .block();
-
-        // JSON 문자열을 JSONObject로 변환하여 totalCount 값 Parsing
-        JSONParser parser = new JSONParser();
-        JSONObject jsonObject = (JSONObject) parser.parse(hospitalInfo);
-        JSONObject response = (JSONObject) jsonObject.get("response");
-        JSONObject body = (JSONObject) response.get("body");
-
-        int totalCount = ((Long) body.get("totalCount")).intValue();
-        int numOfRows = ((Long) body.get("numOfRows")).intValue();
-        int totalPage = (int) Math.ceil(totalCount / numOfRows);
-
-        for (int i = 1; i <= totalPage; i++) {
-            int pageNo = i;
-
-            HospitalItems hospitalItems = webClient.get()
+    public void saveHospitalInfo(int pageNo) {
+        HospitalItems hospitalItems = webClient.get()
                 .uri(uriBuilder -> uriBuilder
-                    .path(HOSPITAL_ENDPOINT)
-                    .queryParam("serviceKey", HOSPITAL_API_KEY)
-                    .queryParam("_type", "json")
-                    .queryParam("pageNo", pageNo)
-                    .build())
+                        .path(HOSPITAL_ENDPOINT)
+                        .queryParam("serviceKey", HOSPITAL_API_KEY)
+                        .queryParam("_type", "json")
+                        .queryParam("pageNo", pageNo)
+                        .build())
                 .accept(MediaType.APPLICATION_JSON)
                 .retrieve()
                 .bodyToMono(HospitalItems.class)
                 .retry(3)
                 .block();
 
-            try {
-                hospitalItems.getHospitalItems().forEach(item -> {
-                    Hospital hospital = item.toEntity();
-                    hospitalRepository.save(hospital);
-                });
-            } catch (DataIntegrityViolationException e) {
-            }
-
-            log.info("pageNo: {} :: hospitalItems: {}", i, hospitalItems);
+        try {
+            hospitalItems.getHospitalItems().forEach(item -> {
+                Hospital hospital = item.toEntity();
+                hospitalRepository.save(hospital);
+            });
+        } catch (DataIntegrityViolationException e) {
         }
+
+        log.info("pageNo: {} :: hospitalItems: {}", pageNo, hospitalItems);
     }
 }
--- src/main/java/dac2dac/doctect/agency/service/PharmacyService.java
+++ src/main/java/dac2dac/doctect/agency/service/PharmacyService.java
@@ -5,9 +5,6 @@ import dac2dac.doctect.agency.repository.PharmacyRepository;
 import dac2dac.doctect.agency.vo.PharmacyItems;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.MediaType;
@@ -30,52 +27,28 @@ public class PharmacyService {
     private String PHARMACY_ENDPOINT;
 
     @Async
-    public void saveAllPharmacyInfo() throws ParseException {
-        String pharmacyInfo = webClient.get()
-            .uri(uriBuilder -> uriBuilder
-                .path(PHARMACY_ENDPOINT)
-                .queryParam("serviceKey", PHARMACY_API_KEY)
-                .queryParam("_type", "json")
-                .build())
-            .retrieve()
-            .bodyToMono(String.class)
-            .block();
-
-        // JSON 문자열을 JSONObject로 변환하여 totalCount 값 Parsing
-        JSONParser parser = new JSONParser();
-        JSONObject jsonObject = (JSONObject) parser.parse(pharmacyInfo);
-        JSONObject response = (JSONObject) jsonObject.get("response");
-        JSONObject body = (JSONObject) response.get("body");
-
-        int totalCount = ((Long) body.get("totalCount")).intValue();
-        int numOfRows = ((Long) body.get("numOfRows")).intValue();
-        int totalPage = (int) Math.ceil(totalCount / numOfRows);
-
-        for (int i = 1; i <= totalPage; i++) {
-            int pageNo = i;
-
-            PharmacyItems pharmacyItems = webClient.get()
+    public void savePharmacyInfo(int pageNo) {
+        PharmacyItems pharmacyItems = webClient.get()
                 .uri(uriBuilder -> uriBuilder
-                    .path(PHARMACY_ENDPOINT)
-                    .queryParam("serviceKey", PHARMACY_API_KEY)
-                    .queryParam("_type", "json")
-                    .queryParam("pageNo", pageNo)
-                    .build())
+                        .path(PHARMACY_ENDPOINT)
+                        .queryParam("serviceKey", PHARMACY_API_KEY)
+                        .queryParam("_type", "json")
+                        .queryParam("pageNo", pageNo)
+                        .build())
                 .accept(MediaType.APPLICATION_JSON)
                 .retrieve()
                 .bodyToMono(PharmacyItems.class)
                 .retry(3)
                 .block();
 
-            try {
-                pharmacyItems.getPharmacyItems().forEach(item -> {
-                    Pharmacy pharmacy = item.toEntity();
-                    pharmacyRepository.save(pharmacy);
-                });
-            } catch (DataIntegrityViolationException e) {
-            }
-
-            log.info("pageNo: {} :: hospitalItems: {}", i, pharmacyItems);
+        try {
+            pharmacyItems.getPharmacyItems().forEach(item -> {
+                Pharmacy pharmacy = item.toEntity();
+                pharmacyRepository.save(pharmacy);
+            });
+        } catch (DataIntegrityViolationException e) {
         }
+
+        log.info("pageNo: {} :: pharmacyItems: {}", pageNo, pharmacyItems);
     }
 }
