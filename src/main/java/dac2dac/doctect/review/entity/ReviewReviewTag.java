63c9c736c50af450e92ec4f8c3c1452af40f21a8
jyjyjy25 <jyjyjy7418@gmail.com>
2024-06-07 19:19:24 +0900

    Feat: 비대면진료 관련 엔티티 재설계 (#13)

--- src/main/java/dac2dac/doctect/review/entity/ReviewReviewTag.java
+++ src/main/java/dac2dac/doctect/review/entity/ReviewReviewTag.java
@@ -1,17 +1,12 @@
 package dac2dac.doctect.review.entity;
 
-import dac2dac.doctect.common.entity.BaseEntity;
-import dac2dac.doctect.doctor.entity.Doctor;
-import dac2dac.doctect.noncontact_diag.entity.NoncontactDiag;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.Lob;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToOne;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,23 +14,18 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Review extends BaseEntity {
+public class ReviewReviewTag {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @OneToOne
-    @JoinColumn(name = "noncontact_diag_id")
-    private NoncontactDiag noncontactDiag;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "review_id")
+    private Review review;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "doctor_id")
-    private Doctor doctor;
-
-    private Double rating;
-
-    @Lob
-    private String content;
+    @JoinColumn(name = "review_tag_id")
+    private ReviewTag reviewTag;
 
 }
