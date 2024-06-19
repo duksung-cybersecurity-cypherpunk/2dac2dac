e98a138ba39b2200acf3e77b6e85761807b94792
jyjyjy25 <jyjyjy7418@gmail.com>
2024-06-19 16:19:08 +0900

    Feat: 비대면진료 관련 엔티티 재설계 (#15)

---
+++ src/main/java/dac2dac/doctect/noncontact_diag/entity/Symptom.java
@@ -0,0 +1,61 @@
+package dac2dac.doctect.noncontact_diag.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Symptom {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Boolean isPrescribedDrug;
+    private Boolean isAllergicSymtom;
+    private Boolean isInbornDisease;
+
+    @Lob
+    private String prescribedDrug;
+
+    @Lob
+    private String allergicSymptom;
+
+    @Lob
+    private String inbornDisease;
+
+    @Lob
+    private String additionalInformation;
+
+    private String symptomImage1;
+    private String symptomImage2;
+    private String symptomImage3;
+    private String symptomImage4;
+    private String symptomImage5;
+
+    @Builder
+    public Symptom(Boolean isPrescribedDrug, Boolean isAllergicSymtom, Boolean isInbornDisease, String prescribedDrug, String allergicSymptom, String inbornDisease,
+        String additionalInformation,
+        String symptomImage1, String symptomImage2, String symptomImage3, String symptomImage4, String symptomImage5) {
+        this.isPrescribedDrug = isPrescribedDrug;
+        this.isAllergicSymtom = isAllergicSymtom;
+        this.isInbornDisease = isInbornDisease;
+        this.prescribedDrug = prescribedDrug;
+        this.allergicSymptom = allergicSymptom;
+        this.inbornDisease = inbornDisease;
+        this.additionalInformation = additionalInformation;
+        this.symptomImage1 = symptomImage1;
+        this.symptomImage2 = symptomImage2;
+        this.symptomImage3 = symptomImage3;
+        this.symptomImage4 = symptomImage4;
+        this.symptomImage5 = symptomImage5;
+    }
+}
