d73a05552cbebac2eb94713d21aeb5988aecc95d
JiYun Kim <jyjyjy7418@gmail.com>
2024-07-20 14:25:28 +0900

    Feat: BillingKeyDto Validation 추가 (#15)

--- src/main/java/dac2dac/doctect/bootpay/dto/request/BootpayBiilingKeyDto.java
+++ src/main/java/dac2dac/doctect/bootpay/dto/request/BootpayBiilingKeyDto.java
@@ -1,5 +1,7 @@
 package dac2dac.doctect.bootpay.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -7,9 +9,23 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class BootpayBiilingKeyDto {
 
+    @NotBlank(message = "카드 번호는 필수입니다.")
+    @Pattern(regexp = "^[0-9]{16}$", message = "카드 번호는 16자리 숫자여야 합니다.")
     private String cardNo;
+
+    @NotBlank(message = "카드 비밀번호는 필수입니다.")
+    @Pattern(regexp = "^[0-9]{2}$", message = "카드 비밀번호 앞자리는 2자리여야 합니다.")
     private String cardPw;
+
+    @NotBlank(message = "신분 번호는 필수입니다.")
+    @Pattern(regexp = "^[0-9]{6}$", message = "생년월일은 6자리 숫자여야 합니다.")
     private String cardIdentityNo;
+
+    @NotBlank(message = "만료 연도는 필수입니다.")
+    @Pattern(regexp = "^[0-9]{2}$", message = "만료 연도는 2자리 숫자여야 합니다.")
     private String cardExpireYear;
+
+    @NotBlank(message = "만료 월은 필수입니다.")
+    @Pattern(regexp = "^(0[1-9]|1[0-2])$", message = "만료 월은 01에서 12 사이의 숫자여야 합니다.")
     private String cardExpireMonth;
 }
