574df53a856d3f9b78246df57a737b9fffef9947
soochan <102716244+soooochan@users.noreply.github.com>
2024-08-05 00:31:24 +0900

    Update WebConfig.java

--- src/main/java/dac2dac/doctect/config/WebConfig.java
+++ src/main/java/dac2dac/doctect/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://192.168.219.180:8081")
+                        .allowedOrigins("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
