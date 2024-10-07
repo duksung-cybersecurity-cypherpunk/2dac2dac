74074c286d52860654563968df2d2dcb36ef663f
jyjyjy25 <jyjyjy7418@gmail.com>
2024-10-08 02:02:57 +0900

    Refactor: 의사 컨트롤러 분리 (#48)
    의사 단 API를 예약, 로그인/회원가입, 내역으로 구분했다.

--- src/main/java/dac2dac/doctect/doctor/controller/DoctorController.java
+++ src/main/java/dac2dac/doctect/doctor/controller/DoctorController.java
@@ -3,28 +3,13 @@ package dac2dac.doctect.doctor.controller;
 
 import dac2dac.doctect.common.constant.SuccessCode;
 import dac2dac.doctect.common.response.ApiResult;
-import dac2dac.doctect.doctor.dto.CustomDoctorDetails;
-import dac2dac.doctect.doctor.dto.DoctorDTO;
 import dac2dac.doctect.doctor.dto.request.DiagCompleteRequestDto;
 import dac2dac.doctect.doctor.dto.request.RejectReservationRequest;
-import dac2dac.doctect.doctor.entity.Doctor;
-import dac2dac.doctect.doctor.repository.DoctorRepository;
 import dac2dac.doctect.doctor.service.DoctorService;
-import dac2dac.doctect.user.controller.LoginController;
-import dac2dac.doctect.user.dto.UserDTO;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.constraints.Pattern;
-import java.util.HashMap;
-import java.util.Map;
 import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -32,16 +17,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Tag(name = "예약", description = "의사 예약 조회 API")
+@Tag(name = "(의사) 진료 예약", description = "의사 진료 예약 관련 API")
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/reservations")
+@RequestMapping("/api/v1/doctors/reservations")
 class DoctorController {
 
     private final DoctorService doctorService;
-    private final DoctorRepository doctorRepository;
-
-    private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
 
     @Operation(summary = "날짜별 예약 조회 API", description = "날짜별 요청된 예약과 수락된 예약을 조회한다.")
     @GetMapping("/{doctorId}/{reservationDate}")
@@ -94,64 +76,5 @@ class DoctorController {
         return ApiResult.success(SuccessCode.GET_SUCCESS, doctorService.getTodayReservation(doctorId));
     }
 
-    @Operation(summary = "진료 완료 내역 조회 API", description = "진료가 완료된 내역을 조회한다.")
-    @GetMapping("/completed/{doctorId}")
-    public ApiResult getCompletedReservation(@PathVariable Long doctorId) {
-        return ApiResult.success(SuccessCode.GET_SUCCESS, doctorService.getCompletedReservation(doctorId));
-    }
-
-
-    @PostMapping("/doctors/signup")
-    public ResponseEntity<String> registerDoctor(@RequestBody DoctorDTO doctorDTO) {
-
-        // 이메일 중복 체크
-        if (doctorRepository.findByEmail(doctorDTO.getEmail()) != null) {
-            return ResponseEntity.badRequest().body("Email is already registered.");
-        }
-
-        Doctor registeredDoctor = doctorService.registerDoctor(doctorDTO);
-        return ResponseEntity.ok("Doctor registered successfully with ID: " + registeredDoctor.getId());
-    }
-
-
-    @PostMapping("/doctors/login/jwt")
-    public ResponseEntity<String> loginUser(@RequestBody UserDTO userDTO, HttpServletResponse response) {
-        try {
-            String token = doctorService.authenticateAndGenerateToken(userDTO.getUsername(), userDTO.getPassword());
-            response.addHeader("Authorization", "Bearer " + token);
-            return ResponseEntity.ok("Login successful");
-        } catch (Exception e) {
-            logger.error("Error during login: {}", e.getMessage(), e);
-            return ResponseEntity.status(500).body("Login failed: " + e.getMessage());
-        }
-    }
-
-
-    @GetMapping("/doctors/login/jwt")
-    public ResponseEntity<Map<String, String>> getProtectedResource() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-
-        if (authentication != null && authentication.isAuthenticated()) {
-            CustomDoctorDetails customDoctorDetails = (CustomDoctorDetails) authentication.getPrincipal();
-
-            String name = customDoctorDetails.getUsername();
-            String email = customDoctorDetails.getEmail(); // CustomUserDetails에서 이메일 가져오기
-            String oneLiner = customDoctorDetails.getOneLiner();
-            String id = customDoctorDetails.getId();
-
-            Map<String, String> response = new HashMap<>();
-            response.put("username", name);
-            response.put("email", email);
-            response.put("id", id);
-            response.put("oneLiner", oneLiner);
-
-            return ResponseEntity.ok(response);
-
-
-        } else {
-            // 인증되지 않은 경우 오류 응답
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
-        }
-    }
 }
 
