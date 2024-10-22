1420cd25884b9e274e327b20c334cd3c28380e89
jyjyjy25 <jyjyjy7418@gmail.com>
2024-10-23 02:04:40 +0900

    Style: 로그인 버튼 색상 수정

--- app/screens/Login/Login.js
+++ app/screens/Login/Login.js
@@ -79,7 +79,7 @@ const styles = StyleSheet.create({
     paddingHorizontal: 10,
   },
   button: {
-    backgroundColor: "#007BFF",
+    backgroundColor: "#5E9740",
     paddingVertical: 15,
     borderRadius: 5,
   },
